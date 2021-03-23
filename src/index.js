5e6396e0d939a94349d8b9ae950cacf029e945df
sharif <sharifgsc15@gmail.com>
2021-03-23 23:38:35 +0600

    page add

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUyOoUwsAv0fh3bJItgJNApNGQ5ZjDC1E",
+  authDomain: "chatbox-app-c6d25.firebaseapp.com",
+  projectId: "chatbox-app-c6d25",
+  storageBucket: "chatbox-app-c6d25.appspot.com",
+  messagingSenderId: "240938263790",
+  appId: "1:240938263790:web:b1d603534f47887a547fff",
+  measurementId: "G-5563HJFXQD"
+};
+
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
